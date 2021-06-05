199b0e0183f92190ce2ea468ea03632d56f37ec0
Hugh Kristiansen <kaarelinden@hotmail.com>
2021-06-05 18:41:31 +0100

    completed Sea step with changing background image

---
+++ src/Controller.js
@@ -0,0 +1,18 @@
+
+function Controller() {
+  this.initialiseSea();
+}
+
+Controller.prototype = {
+
+  initialiseSea: function(){
+    const interval = window.setInterval(changeBackground, 1000, 'images/water0.png', 'images/water1.png');
+    let c = 0;
+    const backgroundImage = document.getElementById('viewport')
+    function changeBackground(a, b){
+      backgroundImage.style.backgroundImage = c%2===0 ? `url(${b})` : `url(${a})`;
+      
+      c++;
+    }
+  }
+}
